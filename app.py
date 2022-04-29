cbd0a2187049c29ff9413febcb7edf33d18c49ae
yachika.wow@gmail.com <you@example.com>
2022-04-29 21:26:04 +0530

    make it better

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('E:/Notebook python project/machine learning project/movie_list.pkl','rb'))
-similarity = pickle.load(open('E:/Notebook python project/machine learning project/similarity.pkl','rb'))
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
